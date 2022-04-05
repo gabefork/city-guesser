fb5d88170cd180df1e8fa074aa7cf6136f12b9c4
poscobg <>
2022-04-05 18:24:22 +0000

    Added United Kingdom and added code to randomly select cities

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import csv
 # CSV importing line from https://stackoverflow.com/a/26179929
 countries = list(csv.reader(open("countries.csv")))
 
-countryNum = random.randint(1,37)
+countryNum = random.randint(0,0)
 country = countries[countryNum][1]
 print(country)
+cityNum = random.randint(2,20)
+city = countries[countryNum][cityNum]
+print(city)
