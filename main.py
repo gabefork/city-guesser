fe984c05f2e7498625038c3244bbe1c599f26814
poscobg <>
2022-04-25 12:34:24 +0000

    Creating a point before I change the structure

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import csv
 # CSV importing line adapted from https://stackoverflow.com/a/26179929
 countries = list(csv.reader(open("countries.csv")))
 score = 0
+correctList = []
 inGame = True
 
-print("Welcome to European City Guesser, a game created to help you get better at GeoGuessr.")
-print("")
+print("Welcome to European City Guesser, a game created to help you get better at GeoGuessr. SImply guess the country that the given city is in, and you'll get a point!")
 
 while inGame == True:
   countryNum = random.randint(0, 36)
@@ -15,17 +15,34 @@ while inGame == True:
   cityNum = random.randint(2, 21)
   city = countries[countryNum][cityNum]
 
-  answer = input("In what country is " + city + "? ")
+  answer = input("\nIn what country is " + city + "? ")
 
   if(answer.casefold() == country.casefold()):
     print("Correct!")
     score += 1
+    correctList.append(city + ", " + country)
   else:
     print("Incorrect! The correct country was " + country + ". Your score was " + repr(score) + ".")
-    again = input("Would you like to play again? ")
+    correctList.append("Ending on " + city + ", " + country)
+    summary = input("\nWould you like a summary? (y/n) ")
+
+    if summary.casefold() == "y":
+      for i in correctList:
+        print(i)
     
-    if again.casefold() == "yes":
-      score = 0
-      inGame = True
+      again = input("Would you like to play again? (y/n) ")
+    
+      if again.casefold() == "y":
+        score = 0
+        inGame = True
+      else:
+        inGame = False
+    
     else:
-      inGame = False
+      again = input("Would you like to play again? (y/n) ")
+    
+      if again.casefold() == "y":
+        score = 0
+        inGame = True
+      else:
+        inGame = False
